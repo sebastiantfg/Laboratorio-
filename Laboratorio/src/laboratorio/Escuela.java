4ed6561192416949070bd09772f1be02b8a81a74
SebastianAsmatVillanueva <sebastianasmat3@gmail.com>
2024-10-11 21:26:30 -0500

    Add files via upload

---
+++ Laboratorio/src/laboratorio/Escuela.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio;
+
+import java.util.List;
+
+/**
+ *
+ * @author PROPIETARIO
+ */
+public class Escuela {
+    private List<Estudiante> estudiantes; // Agregación
+    private List<Profesor> profesores; // Composición
+
+    public List<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(List<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public List<Profesor> getProfesores() {
+        return profesores;
+    }
+
+    public void setProfesores(List<Profesor> profesores) {
+        this.profesores = profesores;
+    }
+}
